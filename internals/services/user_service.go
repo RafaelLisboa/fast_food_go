d373aca547b26fd4e1549b07091bad1f58646b52
RafaelLisboa <laura@PositivoLaura>
2024-07-13 19:55:35 -0300

    feature: password cypher

--- internals/services/user_service.go
+++ internals/services/user_service.go
@@ -2,9 +2,11 @@ package services
 
 import (
 	"context"
+	"crypto/aes"
 	"fast_food_auth/db"
 	"fast_food_auth/internals/models"
 	"fast_food_auth/internals/repositories"
+	"fmt"
 
 	"github.com/google/uuid"
 )
@@ -32,11 +34,26 @@ func (us *userService) CreateUser(ctx context.Context, user models.User) error {
 
 	id := uuid.NewString()
 
+	cypher, err := aes.NewCipher([]byte("hedqieufqworh9238yu3jieiu4fhedw"))
+
+	if err != nil {
+		ctx.Done()
+	}
+
+	fmt.Println(user);
+
+	passwordTextByteBuffer := make([]byte, len(user.Password))
+
+	cypher.Encrypt(passwordTextByteBuffer, []byte(passwordTextByteBuffer))
+
+	passwordText := string(passwordTextByteBuffer)
+
 	userDb := &db.CreateUserParams{
 		ID: id,
 		Name: user.Name,
 		Email: user.Email,
 		Username: user.Username,
+		Password: passwordText,
 	}
 
 	return us.userRepository.CreateUser(ctx, *userDb)
