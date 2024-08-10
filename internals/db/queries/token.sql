5d965270a653de8444d792f2aab0842a600c4733
RafaelLisboa <rafael5tag@hotmail.com>
2024-08-09 23:53:24 -0300

    fix: project structure

--- internals/db/queries/token.sql
+++
@@ -1,6 +0,0 @@
--- name: CreateRefreshToken :exec
-INSERT INTO refresh_tokens (user_id, token, expires_at) VALUES ($1, $2, $3);
-
-
--- name: GetRefreshToken :one
-SELECT token FROM refresh_tokens WHERE token = $1;
